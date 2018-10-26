989b59564b0aa64c2fb52f50426057f499b1a6b4
felix mendoza <felixmendoza@felixs-MacBook-Pro.local>
2018-10-26 09:24:24 -0400

    Devf tool initial commit

---
+++ lib/mendoza_view_tool.rb
@@ -0,0 +1,6 @@
+require "mendoza_view_tool/version"
+require "mendoza_view_tool/renderer"
+
+module MendozaViewTool
+  # Your code goes here...
+end
---
+++ lib/mendoza_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MendozaViewTool
+  VERSION = "0.1.0"
+end
